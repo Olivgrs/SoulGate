59052f8b3a794f6f50b5dfc2f4e2401d5d649aab
alexisBes <alexis.bessonnat@epitech.eu>
2023-01-21 00:56:35 +0100

    Fix Collision event

--- Assets/Sound/CollisionSound.cs
+++ Assets/Sound/CollisionSound.cs
@@ -1,10 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WalkingSound : MonoBehaviour
+public class CollisionSound : MonoBehaviour
 {
-    public Rigidbody2D transform;
     public AudioSource audio;
     // Start is called before the first frame update
     void Start()
@@ -14,11 +14,11 @@ public class WalkingSound : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (audio.isPlaying == false)
-        {
-            audio.volume = Random.Range(0.5f, 1);
-            audio.pitch = Random.Range(0.8f, 1);
-            audio.Play();
-        }
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        audio.Play();
     }
 }
