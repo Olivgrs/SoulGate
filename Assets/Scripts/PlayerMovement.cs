e50d0d83bf2f1ab27ba0959bc1c3b81a4074042d
Quentin Robert <robertquentin29@gmail.com>
2023-01-21 00:14:38 +0100

    Mise en place des controllers avec les flèches et les ZQSD facilement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed;
+    float horizontalMovement, verticalMovement;
 
     public Rigidbody2D rb;
     private Vector3 velocity = Vector3.zero;
 
     void FixedUpdate()
     {
-        float horizontalMovement = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-
-        float verticalMovement = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-
         MovePlayer(horizontalMovement, verticalMovement);
     }
 
@@ -20,6 +18,15 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 targetVelocity = new Vector2(_horizontalMovement, _verticalMovement);
         rb.velocity = Vector3.SmoothDamp(rb.velocity, targetVelocity, ref velocity, .05f);
-        //rb.AddForce(new Vector2(0f, _verticalMovement));
+    }
+
+    public void OnHorizontalMovement(InputValue val)
+    {
+        horizontalMovement = val.Get<float>() * moveSpeed * Time.fixedDeltaTime;
+    }
+
+    public void OnVerticalMovement(InputValue val)
+    {
+        verticalMovement = val.Get<float>() * moveSpeed * Time.fixedDeltaTime;
     }
 }
