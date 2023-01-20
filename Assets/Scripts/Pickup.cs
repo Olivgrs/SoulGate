1a300b9899e48c4b4449a47429f93f7d81b5bd51
olivier <olivier.gurses@epitech.eu>
2023-01-21 00:20:14 +0100

    ADD items

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour {
+
+    private Inventory inventory;
+    public GameObject itemButton;
+    public GameObject effect;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player")) {
+            for (int i = 0; i < inventory.items.Length; i++) {
+                if (inventory.items[i] == 0) {
+                    Instantiate(effect, transform.position, Quaternion.identity);
+                    inventory.items[i] = 1;
+                    Instantiate(itemButton, inventory.slots[i].transform, false);
+                    Destroy(gameObject);
+                    break;
+                }
+            }
+        }
+        
+    }
+}
