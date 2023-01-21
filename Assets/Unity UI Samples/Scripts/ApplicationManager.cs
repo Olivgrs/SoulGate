3f6f4b60c01361d4fa2bfd73dc9773b085fc0f28
olivier <olivier.gurses@epitech.eu>
2023-01-21 18:21:58 +0100

    add menu

---
+++ Assets/Unity UI Samples/Scripts/ApplicationManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ApplicationManager : MonoBehaviour {
+	
+	public GameObject Canva;
+	public void Quit () 
+	{
+		#if UNITY_EDITOR
+		UnityEditor.EditorApplication.isPlaying = false;
+		#else
+		Application.Quit();
+		#endif
+	}
+
+	public void LoadFirstLevel()
+    {
+		Canva.SetActive(false);
+        SceneManager.LoadScene("SolarSystem", LoadSceneMode.Additive);
+    }
+
+
+    public void LoadSecondLevel()
+    {
+		Canva.SetActive(false);
+        SceneManager.LoadScene("SinglePlanet", LoadSceneMode.Additive);
+    }
+}
