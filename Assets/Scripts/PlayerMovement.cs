b9294e49f82081644dd8979a386584a934ae15f7
OlivGrses <olivier.gurses@epitech.eu>
2023-01-22 13:12:54 +0100

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -25,14 +25,14 @@ public class PlayerMovement : MonoBehaviour
     public AudioSource audio;
 
 
-    // Ajoutez une r�f�rence au composant Sprite du joueur
+    // Ajoutez une référence au composant Sprite du joueur
     public SpriteRenderer playerSprite;
-    // Ajoutez une r�f�rence au sprite que vous souhaitez utiliser
+    // Ajoutez une référence au sprite que vous souhaitez utiliser
     public Sprite newSprite;
 
-    // Ajoutez une r�f�rence au composant Animator du joueur
+    // Ajoutez une référence au composant Animator du joueur
     public Animator playerAnimator;
-    // Ajoutez une r�f�rence au nom de l'�tat d'animation associ� au nouveau sprite
+    // Ajoutez une référence au nom de l'état d'animation associé au nouveau sprite
     public string newAnimationState;
 
     public Button play;
@@ -44,11 +44,11 @@ public class PlayerMovement : MonoBehaviour
         text.text = "Il vous faut vite trouver le levier pour ouvrir la porte.";
         if(konamiCode.konamiCodeComplete == true)
         {
-            text.text += " Mais ... Mais... qui �tes vous ?";
+            text.text += " Mais ... Mais... qui êtes vous ?";
             playerSprite.sprite = newSprite;
-            // Changez l'animation du joueur en utilisant la r�f�rence au composant Animator et le nom de l'�tat d'animation
+            // Changez l'animation du joueur en utilisant la référence au composant Animator et le nom de l'êtat d'animation
             playerAnimator.Play(newAnimationState);
-            Debug.Log("Changement effectu�");
+            Debug.Log("Changement effectué");
         }
         //play.onClick.AddListener(UnPause);
         //pause.SetActive(false);
@@ -69,12 +69,12 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(collider.tag);
         if(collider.tag == "Levier" )
         {
-            text.text = "Vous vous trouvez pr�s du levier. Tirer le en appuyant sur E";
+            text.text = "Vous vous trouvez prés du levier. Tirer le en appuyant sur E";
             isInsideLevier = true;
         }
         if (collider.tag == "Porte")
         {
-            text.text = "Vous voici pr�s de la levier. Ouvrez la avec E.";
+            text.text = "Vous voici prés de la levier. Ouvrez la avec E.";
             isInsidePorte = true;
         }
     }
@@ -94,7 +94,7 @@ public class PlayerMovement : MonoBehaviour
     {
         if(isInsideLevier && !levierHasBeenPool)
         {
-            text.text = "Levier acction�. Il vous faut ouvrir la porte sans vous faire attraper par le monstre.";
+            text.text = "Levier acctioné. Il vous faut ouvrir la porte sans vous faire attraper par le monstre.";
             levierHasBeenPool = true;
             LevierSprite.changerSprite();
         }
@@ -103,7 +103,7 @@ public class PlayerMovement : MonoBehaviour
         {
             if(!levierHasBeenPool)
             {
-                text.text = "Vous n'avez pas encore actionn�e le levier.";
+                text.text = "Vous n'avez pas encore actionnée le levier.";
                 return;
             } if (lvl == 0) {
                 Debug.Log("Maze");
