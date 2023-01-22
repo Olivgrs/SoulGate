4de7bc00f8031a68bd58eea27234cfb5aa3af6c3
alexisBes <alexis.bessonnat@epitech.eu>
2023-01-22 18:03:54 +0100

    fix menu

--- Assets/Scripts/UI/PauseScript.cs
+++ Assets/Scripts/UI/PauseScript.cs
@@ -27,7 +27,7 @@ public class PauseScript : MonoBehaviour
 
     public void OnClickMenu()
     {
-        SceneManager.LoadScene("Menu");
+        //SceneManager.LoadScene("Menu");
     }
 
     public void OnClickQuit()
